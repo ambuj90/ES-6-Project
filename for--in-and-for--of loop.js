e7d579a96d8d6c292f6506bc723d64dc79cff303
Ambuj Sharma <33651203+ambuj90@users.noreply.github.com>
2022-06-07 16:47:32 +0530

    Create for--in-and-for--of loop.js

---
+++ for--in-and-for--of loop.js
@@ -0,0 +1,21 @@
+var myFriends = ['Ram','radhay','shyam','hanu'];
+
+for(let elements in myFriends){
+  console.log(elements);
+}
+
+for(let elements of myFriends){
+  console.log(elements);
+}
+
+
+
+// Output:
+// 0
+// 1
+// 2
+// 3
+// vinod
+// ramesh
+// arjun
+// vishal
