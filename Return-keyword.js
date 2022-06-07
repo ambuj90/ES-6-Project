28e8c8fe8131a593ba0f0484413c4d1a75acb223
Ambuj Sharma <33651203+ambuj90@users.noreply.github.com>
2022-06-07 12:18:01 +0530

    Create Return-keyword.js

---
+++ Return-keyword.js
@@ -0,0 +1,14 @@
+// function sum(a, b) {
+//     var total = a + b;
+//     console.log(total);
+// }
+
+// var funExp = sum(5, 15);
+
+function sum(a, b) {
+    return total = a + b;
+}
+
+var funExp = sum(5, 25);
+
+console.log('the sum of two no is ' + funExp);
