62591f70c8969aaa8bdb6c3d563afc69f8dfde40
Ambuj Sharma <33651203+ambuj90@users.noreply.github.com>
2022-06-07 16:57:30 +0530

    Create Array.prototype.forEach().js

---
+++ Array.prototype.forEach().js
@@ -0,0 +1,15 @@
+const array1 = ['a', 'b', 'c'];
+
+array1.forEach(element => console.log(element));
+
+// expected output: "a"
+// expected output: "b"
+// expected output: "c"
+
+
+ var myFriends = ['ambuj','shivani','rohit','madhulika'];
+
+ myFriends.forEach(function(element, index, array) {
+       console.log(element   + " : " + 
+         index  );
+});
