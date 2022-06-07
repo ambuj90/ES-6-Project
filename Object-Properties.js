6ca99305f2d7b9aa74bd074552aed319ea636198
Ambuj Sharma <33651203+ambuj90@users.noreply.github.com>
2022-06-07 15:53:18 +0530

    Create Object-Properties.js

---
+++ Object-Properties.js
@@ -0,0 +1,18 @@
+// ➡ we can now use Dynamic Properties 
+
+// let myName = "Ambuj";
+// const myBio = {
+//   [myName] : "hello how are you?",
+//   [20 + 9] : "is my age"
+// }
+
+// console.log(myBio);
+
+// ➡ no need to write key and value, if both are same 
+
+// let myName = "Ambuj Sharma";
+// let myAge = 29;
+
+// const myBio = {myName,myAge}
+
+// console.log(myBio);
