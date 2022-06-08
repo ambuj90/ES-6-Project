8ab90d395a3241e62b8a6a75f44e0a7fa97ce47d
Ambuj Sharma <33651203+ambuj90@users.noreply.github.com>
2022-06-08 16:46:13 +0530

    Create Array.reverse().js

---
+++ Array.reverse().js
@@ -0,0 +1,13 @@
+// //Array.prototype.reverse() 🙋‍♂️
+// // The reverse() method reverses an array in place. 
+// // The first array element becomes the last, and 
+// // the last array element becomes the first.
+
+const array1 = ['one', 'two', 'three'];
+console.log('array1:', array1);
+
+
+const reversed = array1.reverse();
+console.log('reversed:', reversed);
+
+console.log('array1:', array1);
