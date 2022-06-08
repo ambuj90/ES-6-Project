70c74260ed9612f840f3057b72de43cfc321441c
Ambuj Sharma <33651203+ambuj90@users.noreply.github.com>
2022-06-08 16:12:06 +0530

    Create Array.prototype.includes().js

---
+++ Array.prototype.includes().js
@@ -0,0 +1,19 @@
+const array1 = [1, 2, 3];
+
+console.log(array1.includes(2));
+// expected output: true
+
+const pets = ['cat', 'dog', 'bat'];
+
+console.log(pets.includes('cat'));
+// expected output: true
+
+console.log(pets.includes('at'));
+
+// Array.prototype.includes() 🙋‍♂️
+// Determines whether the array contains a value, 
+// returning true or false as appropriate.
+
+var myFriendNames = ["ambuj","sharma","ambuj","ambujtech"];
+
+console.log(myFriendNames.includes("ambuj"));
