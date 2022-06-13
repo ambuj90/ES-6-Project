d0947742e88655c6c8e40ae57a987fde90d3b731
Ambuj Sharma <33651203+ambuj90@users.noreply.github.com>
2022-06-13 11:44:33 +0530

    Create methods.js

---
+++ methods.js
@@ -0,0 +1,39 @@
+let myName = "ambuj sHarma";
+console.log(myName.toUpperCase());
+console.log(myName.toLowerCase());
+
+
+// The concat() Method 🙋‍♂️
+// concat() joins two or more strings
+
+let fName = "ambuj"
+let lName = "sharma"
+
+console.log(fName + lName );
+console.log(`${fName} ${lName}`);
+console.log(fName.concat(lName));
+console.log(fName.concat(" " ,lName));
+
+
+// String.trim() 🙋‍♂️
+// The trim() method removes whitespace from both 
+// sides of a string
+
+var str = "              Hello         World!            ";
+console.log(str.trim());
+
+
+
+// Converting a String to an Array
+// A string can be converted to an array with the 
+// split() method
+
+var txt = "a, b,c d,e"; // String
+console.log(txt.split(",")); // Split on commas
+console.log(txt.split(" ")); // Split on spaces
+console.log(txt.split("|")); // Split on pipe
+
+
+
+
+
